b7a566581d5669852b8f150086eeff923966c9a9
Erik Costa <erikoliveira.c@gmail.com>
2023-03-09 11:19:53 -0300

    atualização
    função e index.html

--- assets/js/ts.js
+++ assets/js/ts.js
@@ -1,27 +1,27 @@
 function focarXbox(){
     var ts
-    const aviso = document.getElementById("avisoXbox")
+    const aviso = document.getElementById("aviso2")
 
     ts = document.getElementById("btn1")
     ts.addEventListener('focus', aviso.style.display= "grid")
 }
 function focarDisc(){
     var ts
-    const aviso = document.getElementById("aviso")
+    const aviso = document.getElementById("aviso1")
 
     ts = document.getElementById("btn2")
     ts.addEventListener('focus', aviso.style.display= "grid")
 }
-function sair(){
+function sair1(){
     var ts
-    const aviso = document.getElementById("aviso")
+    const aviso = document.getElementById("aviso1")
 
     ts = document.getElementById("btn2")
     ts.addEventListener('mouseleave', aviso.style.display= "none")
 }
-function sair(){
+function sair2(){
     var ts
-    const aviso = document.getElementById("avisoXbox")
+    const aviso = document.getElementById("aviso2")
 
     ts = document.getElementById("btn1")
     ts.addEventListener('mouseleave', aviso.style.display= "none")
