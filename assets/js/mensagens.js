636c2bcb76cf19ca93394f22030f4d746d2f01f3
Erik Costa <erikoliveira.c@gmail.com>
2023-03-17 16:59:02 -0300

    update js

--- assets/js/mensagens.js
+++ assets/js/mensagens.js
@@ -26,13 +26,3 @@ function sair2(){
     ts = document.getElementById("btn1")
     ts.addEventListener('mouseleave', aviso.style.display= "none")
 }
-function claro(){
-    const fundo = document.getElementById("fundoSite")
-    fundo.addEventListener('click', fundo.style.backgroundColor='#f6fff8',
-    fundo.style.color='#011627')
-}
-function escuro(){
-    const fundo = document.getElementById("fundoSite")
-    fundo.addEventListener('click', fundo.style.backgroundColor='#3c3744', 
-    fundo.style.color='#f6fff8')
-}
---
+++ assets/js/tema.js
@@ -0,0 +1,10 @@
+function claro(){
+    const fundo = document.getElementById("fundoSite")
+    fundo.addEventListener('click', fundo.style.backgroundColor='#fdfffc',
+    fundo.style.color='#011627')
+}
+function escuro(){
+    const fundo = document.getElementById("fundoSite")
+    fundo.addEventListener('click', fundo.style.backgroundColor='#111111', 
+    fundo.style.color='#ffffff')
+}
